f2b371081af04ba0e5f2c5d6053f7c5975752de8
Laila-front-dev <your.lailaben@gmail.com>
2024-04-02 23:52:35 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Frontend-Mentor-Single-page-developer-portfolio" 
---
+++ js/main.js
@@ -0,0 +1,91 @@
+const userName = document.querySelector("#username");
+const userEmail = document.querySelector("#useremail");
+const sendMessage = document.querySelector("#sendmessage");
+
+const checkUserName = () => {
+  let valid = false;
+  const min = 3,
+    max = 25;
+  const usernameEl = userName.value.trim();
+
+  if (!isReqiured(usernameEl)) {
+    showError(userName, "Username cannot be blank.");
+  } else if (!isBetween(usernameEl.length, min, max)) {
+    showError(
+      userName,
+      `Username must be between ${min} and ${max} characters.`
+    );
+  } else {
+    showSeccess(userName);
+    valid = true;
+  }
+  return valid;
+};
+
+const checkEmail = () => {
+  let valid = false;
+  const email = userEmail.value.trim();
+
+  if (!isReqiured(email)) {
+    showError(userEmail, "Email cannot be blank.");
+  } else if (!isEmailValid(email)) {
+    showError(userEmail, "Sorry, invalid format here");
+  } else {
+    showSeccess(userEmail);
+    valid = true;
+  }
+  return valid;
+};
+
+const isEmailValid = (email) => {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+};
+
+const isReqiured = (value) => (value === "" ? false : true);
+const isBetween = (lenght, min, max) =>
+  lenght < min || lenght > max ? false : true;
+
+const showError = (input, message) => {
+  const formField = input.parentElement;
+
+  formField.classList.remove("seccess");
+  formField.classList.add("error");
+
+  const error = formField.querySelector("small");
+  error.textContent = message;
+};
+
+const showSeccess = (input) => {
+  const formField = input.parentElement;
+
+  formField.classList.remove("error");
+  formField.classList.add("seccess");
+
+  const error = formField.querySelector("small");
+  error.textContent = "";
+};
+
+sendMessage.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  let isUsernameValid = checkUserName(),
+    isEmailValid = checkEmail();
+
+  let isFormValid = isUsernameValid && isEmailValid;
+
+  if (isFormValid) {
+  }
+});
+
+sendMessage.addEventListener("input", function (e) {
+  switch (e.target.id) {
+    case "usename":
+      checkUserName();
+      break;
+    case "useremail":
+      checkEmail();
+      break;
+  }
+});
